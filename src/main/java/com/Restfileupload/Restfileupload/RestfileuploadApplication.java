2ebfc75a760f29a189b0f67aa9b8465395a3a84d
umarsiddique111 <39054374+umarsiddique111@users.noreply.github.com>
2018-05-16 12:08:29 +0500

    upload project example

---
+++ src/main/java/com/Restfileupload/Restfileupload/RestfileuploadApplication.java
@@ -0,0 +1,12 @@
+package com.Restfileupload.Restfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfileuploadApplication.class, args);
+	}
+}
